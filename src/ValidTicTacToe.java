ba0048a36cfef80d812c921aa17aacc2477fd9b8
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-12 22:59:32 -0700

    added a method which checks all the diagonals and sides for the X and 0 triplet(consecutive)

--- src/ValidTicTacToe.java
+++ src/ValidTicTacToe.java
@@ -1,5 +1,37 @@
 public class ValidTicTacToe {
 
+    boolean checkValidity(String []grid, char ch){
+        if(grid[0].charAt(0)==ch && grid[0].charAt(1)==ch && grid[0].charAt(2)==ch){
+            return true;
+        }
+        if(grid[1].charAt(0)==ch && grid[1].charAt(1)== ch && grid[1].charAt(2)==ch){
+            return true;
+        }
+        if(grid[2].charAt(0)==ch && grid[2].charAt(1)== ch && grid[2].charAt(2)==ch){
+            return true;
+        }
+        if(grid[0].charAt(0)==ch && grid[1].charAt(0)== ch && grid[2].charAt(0)==ch){
+            return true;
+        }
+
+        if(grid[0].charAt(1)==ch && grid[1].charAt(1)== ch && grid[2].charAt(1)==ch){
+            return true;
+        }
+        if(grid[0].charAt(2)==ch && grid[1].charAt(2)== ch && grid[2].charAt(2)==ch){
+            return true;
+        }
+        if(grid[0].charAt(0)==ch && grid[1].charAt(1)== ch && grid[2].charAt(2)==ch){
+            return true;
+        }
+        if(grid[0].charAt(2)==ch && grid[1].charAt(1)== ch && grid[2].charAt(0)==ch){
+            return true;
+        }
+
+        return false;
+    }
+    public static boolean validTicTacToe(String [] board){
+
+    }
 
 
 
